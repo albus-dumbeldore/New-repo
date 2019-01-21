d643cb687411fb816546a3e37a7cfd5101b4281e
albus-dumbeldore <dhruv0426@gmail.com>
2019-01-21 21:04:48 +0530

    merge together

--- file2.js
+++ file2.js
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
 console.log("Now er are at greeter")
-=======
+======
 console.log("console.log we are learning git")
 console.log("git status")
 console.log("use git log to see history")
